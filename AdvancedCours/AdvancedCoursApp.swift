a889eb639811f4a64d25335c48f3836c8115bd80
Alan <yashikalana@mail.ru>
2025-11-23 19:55:51 +0300

    Initial Commit

---
+++ AdvancedCours/AdvancedCoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCoursApp.swift
+//  AdvancedCours
+//
+//  Created by Алан Парастаев on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
